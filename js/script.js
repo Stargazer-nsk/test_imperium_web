0d1391b97b7dc769387558130092ad8e4b23b340
Stargazer-nsk <stargazer.nsk@gmail.com>
2026-04-23 22:30:07 +0300

    Popular, adaptive

--- js/script.js
+++ js/script.js
@@ -8,4 +8,24 @@ jQuery(document).ready(($) => {
         offcanvas.show();
         setTimeout(() => $searchInput.focus(), 500);
     });
+
+    $(function() {
+        const carousel = document.getElementById('offersCarousel');
+
+        if (carousel) {
+            // Обновляем счётчик при переключении слайда
+            carousel.addEventListener('slide.bs.carousel', function(event) {
+                const currentSlide = event.to + 1; // Индексы с 0, +1 для пользователя
+                const totalSlides = $('.carousel-item', carousel).length;
+                $('.carousel-current').text(currentSlide);
+                $('.carousel-total').text(totalSlides);
+            });
+        }
+    });
+
+    $('.btn-next').on('click', function() {
+        let track = $('.carousel-track');
+        let cardWidth = $('.carousel-card').first().outerWidth(true); // Ширина карточки
+        track.scrollLeft(track.scrollLeft() + cardWidth); // Листаем на 1 карточку
+    });
 });
